0d8bf7c97db2b58f24efb4f18a02ffacc7b9f2dc
kenjam97 <jam.ken1997@gmail.com>
2025-09-06 12:39:40 +0100

    setup tailwind / daisyui/ icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="text-3xl font-bold underline">
+    <h1>Hello World!</h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>
