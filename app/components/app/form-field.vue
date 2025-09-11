7291cd426e41307b7ee501b53aa2281187380af4
kenjam97 <jam.ken1997@gmail.com>
2025-09-12 00:22:31 +0100

    add location creation flow, handle csrf tokens

---
+++ app/components/app/form-field.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { type = "text" } = defineProps<{
+  label: string;
+  name: string;
+  type?: "text" | "number" | "textarea";
+  error?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ label }}
+    </legend>
+    <Field
+      :as="type === 'textarea' ? 'textarea' : 'input'"
+      :name="name"
+      :type="type"
+      :disabled="disabled"
+      class="w-full"
+      :class="{
+        'input-error': error,
+        'input': type === 'text' || type === 'number',
+        'textarea': type === 'textarea' }"
+    />
+    <p v-if="error" class="label text-error">
+      {{ error }}
+    </p>
+  </fieldset>
+</template>
