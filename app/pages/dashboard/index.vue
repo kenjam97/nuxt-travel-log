df35966cd101adb72da10519443a0849252e172e
kenjam97 <jam.ken1997@gmail.com>
2025-09-21 15:35:21 +0100

    display locations in the sidebar

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-const { data, status } = await useFetch("/api/locations", { lazy: true },
-);
+const locationStore = useLocationStore();
+const { locations, status } = storeToRefs(locationStore);
+
+onMounted(() => {
+  locationStore.refresh();
+});
 </script>
 
 <template>
@@ -11,9 +15,9 @@ const { data, status } = await useFetch("/api/locations", { lazy: true },
     <div v-if="status === 'pending'">
       <span class="loading loading-spinner loading-xl" />
     </div>
-    <div v-else-if="data && data.length > 0" class="flex flex-wrap mt-4 gap-2">
+    <div v-else-if="locations && locations.length > 0" class="flex flex-wrap mt-4 gap-2">
       <div
-        v-for="location in data"
+        v-for="location in locations"
         :key="location.id"
         class="card card-compact bg-base-300 h-40 w-72"
       >
